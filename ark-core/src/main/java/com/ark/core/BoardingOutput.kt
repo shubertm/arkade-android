245e1907b3cd609eef176d6b361c7ff8f25d5f70
Shubert Munthali <shubertm.m@gmail.com>
2026-03-11 23:25:43 +0200

    Add more methods to BoardingInput

--- ark-core/src/main/java/com/ark/core/BoardingOutput.kt
+++ ark-core/src/main/java/com/ark/core/BoardingOutput.kt
@@ -9,14 +9,73 @@ import fr.acinq.bitcoin.PublicKey
 import fr.acinq.bitcoin.Script
 import fr.acinq.bitcoin.ScriptTree
 import fr.acinq.bitcoin.XonlyPublicKey
+import java.lang.Math.multiplyExact
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 data class BoardingOutput(
-    val severPubKey: XonlyPublicKey,
+    val serverPubKey: XonlyPublicKey,
     val ownerPubKey: XonlyPublicKey,
     val spendingInfo: TaprootSpendingInfo,
     val address: Address,
     val exitDelay: Long,
 ) {
+    private val tapScripts: List<ByteArray>
+
+    init {
+        val forfeitScript = multisigScript(serverPubKey, ownerPubKey)
+        val exitScript = csvSigScript(exitDelay, ownerPubKey)
+        tapScripts = listOf(forfeitScript, exitScript)
+    }
+
+    fun getScriptPubKey() = address.toScriptPubKey()
+
+    fun getArkAddress(
+        network: Network,
+        serverPubKey: XonlyPublicKey,
+    ) = ArkAddress.create(
+        network,
+        serverPubKey.value.toByteArray(),
+        spendingInfo.outputKey.value.toByteArray(),
+    )
+
+    fun getControlBlock(script: ByteArray): ByteArray {
+        val spendingLeaf =
+            spendingInfo.merkleScriptTree.findScript((ByteVector(script)))
+                ?: throw IllegalArgumentException("Invalid leaf script")
+
+        return Script.ControlBlock
+            .build(
+                spendingInfo.internalKey,
+                spendingInfo.merkleScriptTree,
+                spendingLeaf,
+            ).toByteArray()
+    }
+
+    fun getForfeitSpendingInfo(): ScriptSpendingPath {
+        val script = tapScripts[0]
+        val controlBlock = getControlBlock(script)
+        return ScriptSpendingPath(script, controlBlock)
+    }
+
+    fun getExitSpendingInfo(): ScriptSpendingPath {
+        val script = tapScripts[1]
+        val controlBlock = getControlBlock(script)
+        return ScriptSpendingPath(script, controlBlock)
+    }
+
+    fun getTapScripts(): List<ByteArray> = tapScripts
+
+    fun canBeClaimedUnilaterally(
+        now: Duration,
+        blockConfirmTime: Duration,
+    ): Boolean {
+        val exitDelaySeconds = multiplyExact(exitDelay, 512)
+        val exitTime = blockConfirmTime + exitDelaySeconds.toDuration(DurationUnit.SECONDS)
+        return now >= exitTime
+    }
+
     companion object {
         fun create(
             severPubKey: XonlyPublicKey,
