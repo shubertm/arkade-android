61cdf8421cf66f273d4083779211158f80eceabb
Shubert Munthali <shubertm.m@gmail.com>
2026-03-30 06:49:02 +0200

    Add timeouts to OkHttp gRPC client

--- arkade/src/jvmMain/java/com/arkade/network/grpc/ArkServiceClientImpl.jvm.kt
+++ arkade/src/jvmMain/java/com/arkade/network/grpc/ArkServiceClientImpl.jvm.kt
@@ -3,6 +3,7 @@ package com.arkade.network.grpc
 import com.squareup.wire.GrpcClient
 import okhttp3.OkHttpClient
 import okhttp3.Protocol
+import kotlin.time.Duration.Companion.seconds
 
 actual fun gRPCClient(baseUrl: String): GrpcClient =
     GrpcClient
@@ -11,6 +12,9 @@ actual fun gRPCClient(baseUrl: String): GrpcClient =
             OkHttpClient
                 .Builder()
                 .protocols(listOf(Protocol.HTTP_2, Protocol.HTTP_1_1))
+                .callTimeout(60.seconds)
+                .readTimeout(30.seconds)
+                .connectTimeout(30.seconds)
                 .build(),
         ).baseUrl(baseUrl)
         .build()
