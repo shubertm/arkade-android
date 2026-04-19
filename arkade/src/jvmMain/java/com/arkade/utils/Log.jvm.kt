d42f8c4e94d27d1b19a38cc7884be62d1cdc79e4
Shubert Munthali <shubertm.m@gmail.com>
2026-04-19 20:22:45 +0200

    chore: Logging (#37)
    * Add Logging
    
    * Apply fix and nit

---
+++ arkade/src/jvmMain/java/com/arkade/utils/Log.jvm.kt
@@ -0,0 +1,39 @@
+package com.arkade.utils
+
+import java.util.logging.Level
+import java.util.logging.Logger
+
+actual fun Log.debug(
+    tag: String,
+    message: String,
+) {
+    Logger.getLogger(tag).log(Level.FINE, message)
+}
+
+actual fun Log.info(
+    tag: String,
+    message: String,
+) {
+    Logger.getLogger(tag).info(message)
+}
+
+actual fun Log.warning(
+    tag: String,
+    message: String,
+) {
+    Logger.getLogger(tag).warning(message)
+}
+
+actual fun Log.verbose(
+    tag: String,
+    message: String,
+) {
+    Logger.getLogger(tag).log(Level.FINEST, message)
+}
+
+actual fun Log.error(
+    tag: String,
+    message: String,
+) {
+    Logger.getLogger(tag).log(Level.SEVERE, message)
+}
---
+++ arkade/src/nativeMain/kotlin/com/arkade/utils/Log.native.kt
@@ -0,0 +1,33 @@
+@file:Suppress("UNUSED_PARAMETER")
+
+package com.arkade.utils
+
+actual fun Log.debug(
+    tag: String,
+    message: String,
+) {
+}
+
+actual fun Log.info(
+    tag: String,
+    message: String,
+) {
+}
+
+actual fun Log.warning(
+    tag: String,
+    message: String,
+) {
+}
+
+actual fun Log.verbose(
+    tag: String,
+    message: String,
+) {
+}
+
+actual fun Log.error(
+    tag: String,
+    message: String,
+) {
+}
