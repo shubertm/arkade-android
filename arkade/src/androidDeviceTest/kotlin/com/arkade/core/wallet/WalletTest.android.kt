f75f8b8484a04b0ab5062625d999132f62cdefc8
Shubert Munthali <shubertm.m@gmail.com>
2026-05-04 12:33:33 +0200

    refactor: Clean androidDeviceTest (#50)
    * Clean androidDeviceTest
    
    * Change to docs

--- arkade/src/androidDeviceTest/kotlin/com/arkade/core/wallet/WalletTest.android.kt
+++ arkade/src/androidDeviceTest/kotlin/com/arkade/core/wallet/WalletTest.android.kt
@@ -2,20 +2,10 @@ package com.arkade.core.wallet
 
 import com.arkade.storage.db.Database
 import com.arkade.storage.db.initializeTestDb
-import kotlin.test.AfterTest
 import kotlin.test.Test
 
 actual abstract class WalletTest actual constructor() : com.arkade.Test() {
     actual val testDb: Database = initializeTestDb()
-
-    /**
-     * Executes after each test to perform test-specific cleanup.
-     *
-     * Implementations must release resources and restore any shared state created or modified by
-     * the test.
-     */
-    @AfterTest
-    actual abstract fun cleanup()
 
     /**
      * Verifies that creating a wallet succeeds and the created wallet is persisted and retrievable.
